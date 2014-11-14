0d4845d3167b6668f24cab7dd197845139457894
daniel <daniel@nergar-pc>
2014-11-13 19:43:13 -0600

    classes done

--- src/com/bermudi/fifi/TeamManager.java
+++ src/com/bermudi/fifi/TeamManager.java
@@ -1,26 +1,59 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 daniel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.bermudi.fifi;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 /**
  *
  * @author daniel
  */
-public class TeamManager {
-    public void saveTeam(Team team) {
+public final class TeamManager{
+    private static final String DIR = Team.TEAM_LOC;
+    private static final String EXT = ".fifo";
+    
+    public static void saveTeam(Team team) throws FileNotFoundException, IOException {
         
-        String name, crest;
-        int id;
-        float rating;
+        String id = Integer.toString(team.getID());
         
-        name = team.getName();
-        crest = team.getCrest();
-        id = team.getID();
-        rating = team.getRating();
+        FileOutputStream fs = new FileOutputStream(DIR+id+EXT);
+        ObjectOutputStream os = new ObjectOutputStream(fs);
+        os.writeObject(team);
+        os.close();
+        fs.close();
         
+    }
+    
+    public static Team loadTeam(Team team) throws FileNotFoundException, IOException, ClassNotFoundException {
+        
+        String id = Integer.toString(team.getID());
+        
+        FileInputStream fs = new FileInputStream(DIR+id+EXT);
+        ObjectInputStream os = new ObjectInputStream(fs);
+        team = (Team)os.readObject();
+        os.close();
+        fs.close();
+        
+        return team;
         
     }
 }
---
+++ src/com/bermudi/fifi/TestMain.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2014 daniel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.bermudi.fifi;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author daniel
+ */
+public class TestMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        
+        Team team1 = new Team("Manchester United", 4.5f);
+        Team team2 = new Team("Real Madrid FC", 4.5f);
+        Team team3 = new Team("PSG FC", 4.5f);
+        Team team4 = new Team("Liverpool FC", 4f);
+        Team team5 = new Team("Arsenal FC", 5f);
+        Team team6 = new Team("FC Barcelona", 5f);
+        Team team7 = new Team("Chelsea FC", 5f);
+        Team team8 = new Team("Tottenham Hotspur", 5f);
+        Team team9 = new Team("Milan AC", 5f);
+        Team team10 = new Team("Juventus FC", 5f);
+        Team team11 = new Team("Inter Milan", 5f);
+        Team team12 = new Team("Paris Saint Germain", 5f);
+        Team team13 = new Team("Atletico Madrid", 5f);
+        Team team14 = new Team("Borussia Dortmund", 5f);
+        Team team15 = new Team("FC Bayern Munich", 5f);
+        Team team16 = new Team("Galatasaray SK", 5f);
+        
+        TeamManager.saveTeam(team1);
+        TeamManager.saveTeam(team2);
+        TeamManager.saveTeam(team3);
+        TeamManager.saveTeam(team4);
+        TeamManager.saveTeam(team5);
+        TeamManager.saveTeam(team6);
+        TeamManager.saveTeam(team7);
+        TeamManager.saveTeam(team8);
+        TeamManager.saveTeam(team9);
+        TeamManager.saveTeam(team10);
+        TeamManager.saveTeam(team11);
+        TeamManager.saveTeam(team12);
+        TeamManager.saveTeam(team13);
+        TeamManager.saveTeam(team14);
+        TeamManager.saveTeam(team15);
+        TeamManager.saveTeam(team16);
+        
+        
+//        Team team7 = new Team();
+//        Team team8 = new Team();
+//        Team team9 = new Team();
+//        Team team10 = new Team();
+//        Team team11 = new Team();
+//        Team team12 = new Team();
+//        
+//        team7 = TeamManager.loadTeam(team1);
+//        team8 = TeamManager.loadTeam(team2);
+//        team9 = TeamManager.loadTeam(team3);
+//        team10 = TeamManager.loadTeam(team4);
+//        team11 = TeamManager.loadTeam(team5);
+//        team12 = TeamManager.loadTeam(team6);
+        
+    }
+    
+//    public static void main(String[] args) {
+//        int sel;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please select an item from the menu");
+//
+//        while (true) {
+//
+//            System.out.println("1. Show Teams");
+//            System.out.println("2. Add Team");
+//            System.out.println("3. Delete Team");
+//            System.out.println("9. Exit");
+//
+//            sel = input.nextInt();
+//
+//            switch (sel) {
+//                case 1:
+//
+//                    break;
+//                case 2:
+//                    //
+//                    break;
+//                case 3:
+//                    //
+//                    break;
+//                case 9:
+//                    return;
+//                default:
+//                    System.out.println("Try again smart ass");
+//                    System.out.println();
+//            }
+//        }
+//
+//    }
+//
+//    
+
+}
